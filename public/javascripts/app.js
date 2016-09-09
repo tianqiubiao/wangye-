d1ac4089ddf9259fcd678c348a6f336fcc179462
tianqiubiao <tianqiubiao@xjfbank.com>
2016-09-09 17:22:11 +0800

    :aa

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -4,18 +4,7 @@
 
 
 /*设置根字体，自适应*/
-;(function (doc, win) {
-    var docEl     = doc.documentElement,statWidth = docEl.clientWidth,
-        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
-        recalc    = function () {
-            var clientWidth = docEl.clientWidth;
-            if (!clientWidth) return;
-            docEl.style.fontSize = 100 * (clientWidth /360) + 'px';
-        };
-    if (!doc.addEventListener) return;
-    win.addEventListener(resizeEvt, recalc, false);
-    doc.addEventListener('DOMContentLoaded', recalc, false);
-})(document, window);
+
 function query(ele) {
     return document.querySelector(ele)
 }
